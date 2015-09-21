2b5cbbb2691da848a1b8991c90555e6cadd8555a
Kyle Mitchell <kyle@kemitchell.com>
2015-09-20 23:56:22 -0700

    Display term, entities, and additional information

--- index.js
+++ index.js
@@ -2,28 +2,63 @@ var escape = require('xml-escape')
 
 module.exports = ooxmlSignaturePages
 
-var PAGE_BREAK = (
-  [ '<w:pPr>',
-      '<w:r>',
-        '<w:br w:type="page"/>',
-      '</w:r>',
-    '</w:pPr>' ]
-    .join('') )
+function pPr(content) {
+  return ( '<w:pPr>' + content + '</w:pPr>' ) }
+
+function run(content) {
+  return ( '<w:r>' + content + '</w:r>' ) }
+
+function t(text) {
+  return ( '<w:t xml:space="preserve">' + escape(text) + '</w:t>' ) }
+
+var PAGE_BREAK = pPr(run('<w:br w:type="page"/>'))
 
 function header(text) {
-  return (
-    [ '<w:pPr>',
-        '<w:ind w:firstLine="720" />',
-        '<w:jc w:val="start" />',
-        '<w:r>',
-          '<w:t xml:space="preserve">', escape(text), '</w:t>',
-        '</w:r>',
-      '</w:pPr>' ]
-    .join('') ) }
+  return pPr(
+    '<w:ind w:firstLine="720" />' +
+    '<w:jc w:val="start" />' +
+    run(t(text))) }
+
+function paragraph(text) {
+  return pPr(
+    '<w:ind w:left="1440" />' +
+    run(t(text))) }
+
+var BLANKS = '____________________'
+
+var fields = {
+  address: 'Address',
+  date: 'Date',
+  email: 'Email' }
+
+function entityParagraphs(entities) {
+  return entities
+    .reduce(
+      function(returned, element, index, list) {
+        var last = index === ( list.length - 1 )
+        return paragraph(
+          element.name + ', ' +
+          element.jurisdiction + ' ' +
+          element.form +
+          ( last ? '' : ( ', its ' + list[index + 1].role ) )) },
+      [ ]) }
 
 function page(argument) {
   return (
-    header(argument.header) ) }
+    header(argument.header) +
+    ( 'term' in argument ?
+        paragraph(argument.term) : '' ) +
+    ( 'entities' in argument ?
+        entityParagraphs(argument.entities) : '' ) +
+    paragraph('By: ' + BLANKS) +
+    paragraph('Name: ' + argument.name) +
+    ( 'entities' in argument ?
+         paragraph('Title: ' + argument.entities[0].role) : '' ) +
+    ( argument.information ?
+        argument.information
+          .map(function(element) {
+            return paragraph(fields[element] + ': ' + BLANKS) })
+          .join('') : '' ) ) }
 
 function ooxmlSignaturePages(signaturePages) {
   if (!Array.isArray(signaturePages)) {
